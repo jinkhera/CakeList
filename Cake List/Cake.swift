d4c2724dc54ab8d9c6b5f581622537e962530452
Jin Khera <jin.khera@gmail.com>
2019-10-07 12:03:12 +0100

    add Swift briding header and 'Cake' model

---
+++ Cake List/Cake.swift
@@ -0,0 +1,24 @@
+//
+//  Cake.swift
+//  Cake List
+//
+//  Created by Jatinder Pal Singh Khera on 07/10/2019.
+//  Copyright © 2019 Stewart Hart. All rights reserved.
+//
+
+import Foundation
+
+/// As structs are not interoptable with Swift we need to use classes with '@objc' decorators
+@objc class Cake: NSObject {
+    // MARK: - vars
+    var title: String
+    var desc: String
+    var image: URL
+    
+    // MARK: - initialisation
+    public init(title: String, desc: String, imageURL url: URL) {
+        self.title = title
+        self.desc = desc
+        self.image = url
+    }
+}
